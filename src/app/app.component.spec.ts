641fc1c89f4633a8ff5bf56722d66c5c6eeda183
Amanda Kendal-Brown <ajkendal@neiu.edu>
2021-01-26 15:11:45 -0600

    Updates to Test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Publish your podcasts');
+    expect(compiled.querySelector('#mainHeader').textContent).toContain('Publish your podcasts');
   });
 });
