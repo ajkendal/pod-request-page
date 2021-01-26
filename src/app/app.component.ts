0a2d40c3998a66b31c0ee259164373ab97d056db
Amanda Kendal-Brown <ajkendal@neiu.edu>
2021-01-26 15:00:46 -0600

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { ValueConverter } from '@angular/compiler/src/render3/view/template';
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PodRequest';
+
+  email = new FormControl;
+
+  submitEmail(){
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let validation = re.test(String(this.email.value).toLowerCase());
+    let errorDiv = document.getElementById("invalidEmail");
+    if (!validation || this.email.value == null) {
+      errorDiv.style.display = "block";
+    }
+    else {
+      errorDiv.style.display = "none";
+    }
+  }
 }
